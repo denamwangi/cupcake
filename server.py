38c21be81177e387457d77e6586f34c07d0cb822
Katie Byers <lobsterkatie@gmail.com>
2018-08-06 18:13:46 -0700

    slash command working but doesn’t do the thing yet

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from slackclient import SlackClient
 import os
 import pprint
 
 app = Flask(__name__)
-slack_client = SlackClient(os.environ["BOT_USER_ACCESS_TOKEN"])
+
+# TODO probably factor all slack setup stuff out into it's own file/class(?)
+slack_token = os.environ["BOT_USER_ACCESS_TOKEN"]
+slack_client = SlackClient(slack_token)
+
+
+def get_user_info(user_id):
+    return slack_client.api_call(
+        "users.info",
+        user=user_id,
+        token=slack_token
+    )
 
 
 @app.route("/")
@@ -15,6 +26,21 @@ def show_index():
     pprint.pprint(slack_client.api_call("chat.postMessage", channel="CC32SU9DE",
                                         text="Hello from actual cupcake! :tada:", username="cupcake"))
     return "it worked!!!"
+
+
+@app.route("/slash-test", methods=["POST"])
+def handle_slash_command():
+    # import pdb
+    # pdb.set_trace()
+    command_text = request.form.get('text')
+    slack_uid = request.form.get('user_id')
+    # # TODO ^ change the name of this variable - it's the requesting user
+
+    user_info = get_user_info(slack_uid)
+
+    response_body = {"text": "you entered " + command_text}
+
+    return jsonify(response_body)
 
 
 if __name__ == "__main__":
