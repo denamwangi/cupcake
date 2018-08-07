710f8d1641e0f59016d0bdf9beecdc93f6869dde
Katie Byers <lobsterkatie@gmail.com>
2018-08-06 17:23:08 -0700

    posting to slack from app working

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from slackclient import SlackClient
 import os
+import pprint
 
 app = Flask(__name__)
-slack_client = SlackClient(os.environ["SLACK_TEST_TOKEN"])
+slack_client = SlackClient(os.environ["BOT_USER_ACCESS_TOKEN"])
 
 
 @app.route("/")
 def show_index():
-    print slack_client.api_call("api.test")
+    # pprint.pprint(slack_client.api_call("channels.list"))
+
+    # printing so we can see the output - this can be taken out once everything's working
+    pprint.pprint(slack_client.api_call("chat.postMessage", channel="CC32SU9DE",
+                                        text="Hello from actual cupcake! :tada:", username="cupcake"))
     return "it worked!!!"
 
 
