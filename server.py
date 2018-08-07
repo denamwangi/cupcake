f119ed680d48dafd8f3a62727b68c31261207b8c
denamwangi <dena@getsentry.com>
2018-08-06 18:15:38 -0700

    Add github webhooks handling v1

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, json, Response
 from slackclient import SlackClient
 import os
 import pprint
@@ -15,6 +15,31 @@ def show_index():
     pprint.pprint(slack_client.api_call("chat.postMessage", channel="CC32SU9DE",
                                         text="Hello from actual cupcake! :tada:", username="cupcake"))
     return "it worked!!!"
+
+
+@app.route("/github", methods=['POST'])
+def process_github_webhook():
+    # github sends us the issue, sender, repo, and action
+    valid_actions = ['opened', 'closed']
+    if request.method != 'POST':
+        return Response('noooopes\n', status=405)
+
+    start = datetime.utcnow()
+
+    try:
+        data = json.loads(request.data)
+    except Exception:
+        return Response('ayooo this is not json\n', status=400)
+
+    action = data.get('action', None)
+    sender = data.get('sender', None)
+
+    print "HI HI {} just {} an issue in this repo".format(sender['login'], action)
+
+    return Response(status=201, headers=(
+        ('Access-Control-Allow-Origin', '*'),
+    ))
+
 
 
 if __name__ == "__main__":
