f6d6379fbb11cf5849078aeeaf9159099f2f0696
denamwangi <dena@getsentry.com>
2018-08-08 08:59:02 -0700

    move function where it should be

--- server.py
+++ server.py
@@ -19,6 +19,20 @@ def get_user_info(user_id):
         token=slack_token
     )
 
+def get_repo_info():
+    """ Grabs all the authors and their number of commits to the repo
+        returns {'author_name': int no_of_commits}
+    """
+    request_url = 'https://api.github.com/repos/denamwangi/CupCake/commits'
+    response = json.loads(requests.get(request_url).content)
+
+    committers = {}
+    for each_commit in response:
+        # import ipdb; ipdb.set_trace()
+        author = each_commit['author']['login']
+        committers[author] = committers.get(author, 0) + 1
+
+    return committers
 
 def post_to_slack(text):
     """ Convenience method to make it easier to post to slack. Might generalize even more e.g. pass in channel
@@ -72,7 +86,7 @@ def process_github_webhook():
         data = json.loads(request.data)
     except Exception:
         return Response('ayooo this is not json\n', status=400)
-
+    pprint.pprint("Github webhook come thruuuu:", data)
     action = data.get('action', None)
     sender = data.get('sender', None)
 
@@ -84,20 +98,6 @@ def process_github_webhook():
         ('Access-Control-Allow-Origin', '*'),
     ))
 
-def get_repo_info():
-    """ Grabs all the authors and their number of commits to the repo
-        returns {'author_name': int no_of_commits}
-    """
-    request_url = 'https://api.github.com/repos/denamwangi/CupCake/commits'
-    response = json.loads(requests.get(request_url).content)
-
-    committers = {}
-    for each_commit in response:
-        import ipdb; ipdb.set_trace()
-        author = each_commit['author']['login']
-        committers[author] = committers.get(author, 0) + 1
-
-    return committers
 
 @app.route("/slash-test", methods=["POST"])
 def handle_slash_command():
