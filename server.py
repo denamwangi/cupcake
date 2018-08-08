dd0bfda28c367a74f255fd95da61ae973b005dff
Katie Byers <lobsterkatie@gmail.com>
2018-08-07 17:02:49 -0700

    slash command working but brittle

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from slackclient import SlackClient
 import os
+import re
 import pprint
 
 app = Flask(__name__)
@@ -18,27 +19,62 @@ def get_user_info(user_id):
     )
 
 
+def oxfordize(strings):
+    """Given a list of strings, formats them correctly given the length of the
+    list. For example:
+        oxfordize(['A'])  =>  'A'
+        oxfordize(['A', 'B'])  =>  'A and B'
+        oxfordize(['A', 'B', 'C'])  =>  'A, B, and C'
+    """
+
+    if len(strings) == 0:
+        return ''
+    elif len(strings) == 1:
+        return strings[0]
+    elif len(strings) == 2:
+        return '%s and %s' % (strings[0], strings[1])
+    else:
+        return '%s, and %s' % (', '.join(strings[:-1]), strings[-1])
+
+
 @app.route("/")
 def show_index():
     # pprint.pprint(slack_client.api_call("channels.list"))
 
+    return "click <a href='/test-message'>here</a> to send a test message"
+
+
+@app.route("/test-message")
+def send_test_message():
     # printing so we can see the output - this can be taken out once everything's working
-    pprint.pprint(slack_client.api_call("chat.postMessage", channel="CC32SU9DE",
-                                        text="Hello from actual cupcake! :tada:", username="cupcake"))
-    return "it worked!!!"
+    pprint.pprint(
+        slack_client.api_call("chat.postMessage",
+                              channel="CC32SU9DE",
+                              text="Hello from actual cupcake! :tada:", username="cupcake"))
+    return redirect("/")
 
 
 @app.route("/slash-test", methods=["POST"])
 def handle_slash_command():
-    # import pdb
-    # pdb.set_trace()
+    requesting_user_id = request.form.get('user_id')
     command_text = request.form.get('text')
-    slack_uid = request.form.get('user_id')
-    # # TODO ^ change the name of this variable - it's the requesting user
+    mentioned_users = re.findall(r"<@U\S*>", command_text)
 
-    user_info = get_user_info(slack_uid)
+    # TODO enforce the 'for'ness with a try and return error message if it didn't work
+    reason = command_text.split(" for ")[-1]
 
-    response_body = {"text": "you entered " + command_text}
+    # requesting_user_info = get_user_info(requesting_user_id)
+
+    response_body = {
+        "text": "<@{user_id}> gave a cupcake to {users} for {reason}".format(
+            user_id=requesting_user_id,
+            users=oxfordize(mentioned_users),
+            reason=reason),
+        "response_type": "in_channel",
+        "attachments": [{
+            "text": "Thanks for giving cupcakes!"
+        }],
+    }
 
     return jsonify(response_body)
 
