789e497c88b96acae9ab887939ebb91dec52dd89
denamwangi <dena@getsentry.com>
2018-08-08 18:10:59 -0700

    add github api calls for repo reviews

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 slack = SlackHelper(os.environ["BOT_USER_ACCESS_TOKEN"])
 
 
-def get_repo_info():
+def get_repo_commits():
     """ Grabs all the authors and their number of commits to the repo
         returns {'author_name': int no_of_commits}
     """
@@ -24,6 +24,39 @@ def get_repo_info():
     return committers
 
 
+def get_repo_reviews():
+    """ Grabs all the reviewers and the number of reviews to the repo
+        returns {'reviewer_name': int no_of_reviews}
+    """
+    request_url = 'https://api.github.com/repos/denamwangi/skills_flask/pulls?state=all'
+    response = json.loads(requests.get(request_url).content)
+
+    repo_reviewers = {}
+    for each_pr in response:
+        # grab the pr id, pass that to the reviews function, which asks 
+        # for all the reviews for that pr and grabs all the authors of the reviews
+        # import ipdb; ipdb.set_trace()
+        id = each_pr.get('number')
+        reviewers = get_pr_reviews(id)
+        for reviewer in reviewers:
+            repo_reviewers[reviewer] = repo_reviewers.get(reviewer, 0) + 1
+
+    return 'repo_reviewers'
+
+def get_pr_reviews(id):
+    """ Grabs all the reviews for this pr. Returns a set of all reviewers
+    """
+
+    request_url = 'https://api.github.com/repos/denamwangi/skills_flask/pulls/{}/reviews'.format(id)
+
+    response = json.loads(requests.get(request_url).content)
+    pr_reviewers = set()
+    for each_review in response:
+        reviewer = each_review['user']['login']
+        pr_reviewers.add(reviewer)
+    return pr_reviewers
+
+
 @app.route("/")
 def show_index():
     # pprint.pprint(slack_client.api_call("channels.list"))
@@ -32,16 +65,19 @@ def show_index():
 
 @app.route("/github", methods=['POST'])
 def process_github_webhook():
-    # github sends us the issue, sender, repo, and action
-    valid_actions = ['opened', 'closed']
+    # github sends us the event, sender, repo, and action. The type of event is in the header.
+    valid_actions = ['submitted', 'opened', 'closed']
+    valid_events = ['pull_request_review', 'commits']
     if request.method != 'POST':
         return Response('noooopes\n', status=405)
-
+    event_type = request.headers.get('X-Github-Event')
     try:
         data = json.loads(request.data)
     except Exception:
         return Response('ayooo this is not json\n', status=400)
-    pprint.pprint("Github webhook come thruuuu:", data)
+    print "********Github webhook come thruuuu:", data
+
+    # TODO[Dena]: add a check to see if this is the first/nth commit or review
     action = data.get('action', None)
     sender = data.get('sender', None)
 
